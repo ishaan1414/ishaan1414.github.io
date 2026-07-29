0504c9579bc806905adfbc21d75874db0b52a3c4
ishaan1414 <ishaan1414@gmail.com>
2026-07-29 01:42:26 -0400

    File creation for part 3

---
+++ assignment4/part3/main.js
@@ -0,0 +1,23 @@
+/*Name - Ishaan Malhotra
+Date Created - 28 July 2026
+File Name - main.js
+Description - Assignment 4 Part 3 Bouncing Balls for INFT 1206*/
+// setup canvas
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+// function to generate random number
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// function to generate random color
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
