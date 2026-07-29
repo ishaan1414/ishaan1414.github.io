1877c20a0a6b986bfdcae23b1116fa91e2490e59
ishaan1414 <ishaan1414@gmail.com>
2026-07-28 20:27:27 -0400

    File creation and set-up for part 2

---
+++ assignment4/part2/script.js
@@ -0,0 +1,9 @@
+/*Name - Ishaan Malhotra
+Date Created - 28 July 2026
+File Name - script.js
+Description - Assignment 4 Part 2 Image Gallery for INFT 1206 */
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
+
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
