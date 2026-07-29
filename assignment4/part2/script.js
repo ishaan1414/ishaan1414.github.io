eb3c24f11fcf7808e5985c03c5d0f99ded27e08a
ishaan1414 <ishaan1414@gmail.com>
2026-07-29 01:05:29 -0400

    Finished all Parts, Made major fixes
    Finished all the required parts and made major fixes and typos. Need to double check everything and validate HTML.

--- assignment4/part2/script.js
+++ assignment4/part2/script.js
@@ -9,23 +9,35 @@ const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
 
 const images = [
-    {filename: "pic3.jpg", alt: "Closeup of a human eye"},
+    {filename: "pic1.jpg", alt: "Closeup of a human eye"},
     {filename: "pic2.jpg", alt: "Rock that looks like a wave"},
     {filename: "pic3.jpg", alt: "Purple and white pansies"},
     {filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb"},
     {filename: "pic5.jpg", alt: "Large moth on a leaf"}
 ];
 // Loops through all our images and gives us the thumbnails. 
-constbaseURL = "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
-for (const picture of pictures) {
+const baseURL = "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
+for (const picture of images) {
 const newImage = document.createElement("img");
-newImage.src = `${baseURL}${images.filename}`;
-newImage.alt = images.alt;
+newImage.src = `${baseURL}${picture.filename}`;
+newImage.alt = picture.alt;
 newImage.tabIndex = "0";
-thumbBar.addEventListener("click", updateDisplayedImage)
+thumbBar.appendChild(newImage);
+thumbBar.addEventListener("click", updateDisplayedImage);
 }
 // The updateDisplayedImage() function. Displays the activated thumbnail at full size.
 function updateDisplayedImage(e) {
   displayedImage.src = e.target.src;
   displayedImage.alt = e.target.alt;
 }
+// Dark/Light button to switch overlay
+btn.addEventListener("click", () => {
+  if (btn.classList.contains("dark")) {
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = "rgb(0 0 0/0.5)";
+  } else {
+    btn.textContent = "Darken";
+    overlay.style.backgroundColor = "rgb(0 0 0/0)";
+  }
+  btn.classList.toggle("dark");
+});
