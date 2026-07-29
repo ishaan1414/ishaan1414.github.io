4039bc7126bb6b058a8102c7060e277a130638c9
ishaan1414 <ishaan1414@gmail.com>
2026-07-29 06:20:37 -0400

    Update main.js
    Updated Ball to extend Shape and changed collisionDetect() as per MDN

--- assignment4/part4/main.js
+++ assignment4/part4/main.js
@@ -29,14 +29,12 @@ class Shape {
     this.velY = velY;
   }
 }
-class Ball {
+class Ball extends Shape {
   constructor(x, y, velX, velY, color, size) {
-    this.x = x;
-    this.y = y;
-    this.velX = velX;
-    this.velY = velY;
+    super(x, y, velX, velY);
     this.color = color;
     this.size = size;
+    this.exists = true;
   }
   draw() {
     ctx.beginPath();
@@ -65,9 +63,9 @@ class Ball {
     this.x += this.velX;
     this.y += this.velY;
   }
-  collisionDetect() {
+   collisionDetect() {
     for (const ball of balls) {
-      if (this !== ball) {
+      if (!(this === ball) && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
