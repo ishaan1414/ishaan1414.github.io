6258cc6524aea9f45763d313942365b7ab5311b9
ishaan1414 <ishaan1414@gmail.com>
2026-07-29 20:30:47 -0400

    Update main.js
    Finished collisionDetect()

--- assignment4/part4/main.js
+++ assignment4/part4/main.js
@@ -124,7 +124,18 @@ class EvilCircle extends Shape {
   }
 
   collisionDetect(){
-    
+  for (const ball of balls) {
+    if (ball.exists) {
+      const dx = this.x - ball.x;
+      const dy = this.y - ball.y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+      if (distance < this.size + ball.size) {
+          ball.exists = false;
+          counter--;
+          ballCountPara.textContent = `Ball count: ${counter}`;
+        }
+      }
+    }
   }
 }
 const balls = [];
