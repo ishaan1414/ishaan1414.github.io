c714e2e189c5e7b7f6a660a1e7339ce7b0310f81
ishaan1414 <ishaan1414@gmail.com>
2026-07-31 15:21:35 -0400

    Update main.js
    Added the villian ball

--- assignment4/part4/main.js
+++ assignment4/part4/main.js
@@ -167,4 +167,20 @@ function loop() {
 
   requestAnimationFrame(loop);
 }
+const villianBall = new EvilCircle(random(0, width), random(0, height));
+function loop() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillRect(0, 0, width, height);
+  for (const ball of balls) {
+    if (ball.exists) {
+      ball.draw();
+      ball.update();
+      ball.collisionDetect();
+    }
+  }
+  villianBall.draw();
+  villianBall.checkBounds();
+  villianBall.collisionDetect();
+  requestAnimationFrame(loop);
+}
 loop();
