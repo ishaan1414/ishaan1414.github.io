b904964b8a2c9243d345a643afc2fe0f722a2bd8
ishaan1414 <ishaan1414@gmail.com>
2026-07-31 15:48:59 -0400

    Final validation/fixes
    Proofread, validated, and made fixes to the script. Part 4 is good to go

--- assignment4/part4/main.js
+++ assignment4/part4/main.js
@@ -157,20 +157,9 @@ while (balls.length < 25) {
 
   balls.push(ball);
   counter++;
-  parseFloat.textContent = `Ball Count: ${counter}`;
+  ballCountPara.textContent = `Ball Count: ${counter}`;
 }
-function loop() {
-  ctx.fillStyle = "rgb(0 0 0 / 25%)";
-  ctx.fillRect(0, 0, width, height);
 
-  for (const ball of balls) {
-    ball.draw();
-    ball.update();
-    ball.collisionDetect();
-  }
-
-  requestAnimationFrame(loop);
-}
 const villianBall = new EvilCircle(random(0, width), random(0, height));
 function loop() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
